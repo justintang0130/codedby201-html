01cd1c4bb2acaba8adf832db0955115fe137d2fa
Justin Tang <justintang0130@gmail.com>
2025-02-04 23:50:04 +0000

    basic javascript skills

---
+++ .vscode/server.js
@@ -0,0 +1,14 @@
+let age = 16;
+age = 17;
+console.log(age);
+
+age + 3;
+
+console.log(age);
+
+let newstring = "Hello Class!";
+console.log(newstring);
+
+let attended = true;
+attended = false;
+console.log(attended)
---
+++ server.js
@@ -0,0 +1,18 @@
+let a = 100;
+let b = (100 + 50);
+console.log(a * b);
+
+let age = 118;
+console.log(age);
+
+if (age >= 16) {
+    console.log("You can get a driver's license");
+}
+
+else {
+    console.log("You're too young to get a license");
+}
+
+let text ="Hello World";
+console.log(text);
+
